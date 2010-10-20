9da8412abea3cde992031beaaea4a317fad97da8
Bryn Mathias <bm409>
2010-10-20 13:46:13 +0000

    added  cout of Mass of dijet in the case of events passing aT

--- bryn/src/common/HadronicCommonPlots.cc
+++ bryn/src/common/HadronicCommonPlots.cc
@@ -566,13 +566,14 @@ bool HadronicCommonPlots::StandardPlots( Event::Data& ev ) {
       // if(ev.CommonAlphaT() > 0.55 || HadronicCommonPlots::MT2_Leading(ev) > 200. || HadronicCommonPlots::MT2(ev) > 200. || ev.CommonMHT().Pt() > 250.){
         if(ev.CommonAlphaT() > 0.55 && ev.CommonHT() > 350.){
           LorentzV lv1(0.,0.,0.,0.);
+          LorentzV lv2(0.,0.,0.,0.);
           lv1 += *(ev.JD_CommonJets().accepted[0]);
-          lv1 += *(ev.JD_CommonJets().accepted[1]);
+          lv2 += *(ev.JD_CommonJets().accepted[1]);
         cout << " This event passes offline cuts!!! " <<endl;
         cout << "Alpha T is " << ev.CommonAlphaT() << " HT is "<< ev.CommonHT() <<" MHT is  " << ev.CommonMHT().Pt() << " and MT2 is " << HadronicCommonPlots::MT2_Leading(ev) << " Bised delta phi is " << ev.CommonMinBiasDPhi()<< " Meff is " << ev.CommonMHT().Pt()+ev.CommonHT()<<endl;
         if(HadronicCommonPlots::MT2_Leading(ev) > 300.){ cout << "MT2 Variables are: px,py, Leading Jet " <<
           ev.JD_CommonJets().accepted[0]->Px() << "," << ev.JD_CommonJets().accepted[0]->Py() << "," <<" px,py, Second Jet " << ev.JD_CommonJets().accepted[1]->Px()  <<"," <<  ev.JD_CommonJets().accepted[1]->Py() << "," << endl;}
-        cout << "run : " <<  ev.RunNumber() << "lumi Section "<< ev.LumiSection() <<  " event : "<<  ev.EventNumber() << " Alpha T is" << ev.CommonAlphaT() << " Effective mass is : " << ev.CommonMHT().Pt()+ev.CommonHT() << " Dijet Mass is " << (lv1).M() << endl;
+        cout << "run : " <<  ev.RunNumber() << "lumi Section "<< ev.LumiSection() <<  " event : "<<  ev.EventNumber() << " Alpha T is" << ev.CommonAlphaT() << " Effective mass is : " << ev.CommonMHT().Pt()+ev.CommonHT() << " Dijet Mass is " << (lv1+lv2).M() << endl;
         cout << "x-cleaned"<<endl;
         ev.PrintStableVisRec();
         cout << "all"<<endl;
