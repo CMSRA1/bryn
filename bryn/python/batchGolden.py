15d9a498440b1b0e791c7b0ef70f060c1694a443
Bryn Mathias <bm409>
2010-10-29 14:42:56 +0000

    moved to the RA1 samples deffintion

--- bryn/python/batchGolden.py
+++ bryn/python/batchGolden.py
@@ -367,7 +367,7 @@ alphaT1 = OP_CommonAlphaTCut(0.55)
 spikecleaner = OP_EcalSpikeCleaner()
 event_display = OP_EventDisplay("EventDisplays", "common") #to draw all/common objects
 alphat = OP_CommonAlphaTCut(0.55)
-DeadEcalCutData = OP_DeadECALCut(0.3,0.5,30.,10,1,"./deadRegionList_GR10_P_V10.txt")
+DeadEcalCutData = OP_DeadECALCut(0.3,0.5,30.,10,0,"./deadRegionList_GR10_P_V10.txt")
 DeadEcalCutMC =   OP_DeadECALCut(0.3,0.5,30.,10,1,"./deadRegionList_START38_V12.txt")
 
 MHT_METCut = OP_MHToverMET(1.25)
