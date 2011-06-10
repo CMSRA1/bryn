07cfb28744a8cee0fa2fac11ae928e37a2c78cc2
Bryn Mathias <bm409>
2011-06-10 10:54:58 +0000

    add event filter

--- bryn/src/common/Python.cc
+++ bryn/src/common/Python.cc
@@ -3,11 +3,16 @@
 #include "Trigger.hh"
 #include "TriggerTurnOns.hh"
 #include "EventDump.hh"
+#include "EventSelector.hh"
 using namespace boost::python;
 
 BOOST_PYTHON_MODULE(libbryn) {
 
   class_<Operation::WeeklyUpdatePlots, bases<Operation::_Base> >( "WeeklyUpdatePlots",
+                    init<const Utils::ParameterSet&>());
+
+
+  class_<Operation::eventselector_t, bases<Operation::_Base> >( "OP_RunLumiEvSelector",
                     init<const Utils::ParameterSet&>());
 
   class_<Operation::Trigger, bases<Operation::_Base> >( "PL_TriggerPlots",
