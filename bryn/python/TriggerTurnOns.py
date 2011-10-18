679ccd9928933178a80fed71a123ca5edf51387f
Bryn Mathias <bm409>
2011-10-18 15:12:20 +0000

    add meff plots and function for making plot pairs -- this is test version

--- bryn/python/TriggerTurnOns.py
+++ bryn/python/TriggerTurnOns.py
@@ -207,10 +207,10 @@ Cross_Trigger_PS = PSet(
 def AddHistPair(cutTree = None,cut = None, RefTrig = None, TestTrig = None):
   """docstring for AddBinedHist"""
   out = []
-  refPlots = PL_TriggerTurnOns( PSet(DirName = RefTrig+"_For_"+TestTrig,MinObjects =0 ,MaxObjects = 15,Plots = True,ReWeight = True,TriggerReWeight = [RefTrig],Verbose = False).ps())
+  refPlots = PL_TriggerTurnOns( PSet(DirName = RefTrig+"_For_"+TestTrig,MinObjects =0 ,MaxObjects = 15,Plots = True,ReWeight = True,TriggerReWeight = [RefTrig,TestTrig],Verbose = False).ps())
   testTrigPlots = PL_TriggerTurnOns( PSet(DirName = TestTrig+"_From_"+RefTrig,MinObjects =0 ,MaxObjects = 15,Plots = True,ReWeight = True,TriggerReWeight = [TestTrig],Verbose = False).ps())
   refTrigPS =  PSet(Verbose = False,UsePreScaledTriggers = True,Triggers = [] )
-  refTrigPS.Triggers = [RefTrig,TestTrig]
+  refTrigPS.Triggers = [RefTrig]
   refTrigOP = OP_MultiTrigger( refTrigPS.ps() )
   testTrigPS = PSet(Verbose = False,UsePreScaledTriggers = True,Triggers = [] )
   testTrigPS.Triggers = [TestTrig]
